16dcf2064acd149f4105948d7d5838afcaebb2f4
shahanamolb <shahanashahu309@gmail.com>
2022-10-15 13:15:54 +0530

    I have created my first SPA Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const appRoute: Routes =
+[
+  {
+    path:'',
+    component:SigninComponent
+  },
+  {
+    path:'signin',
+    component:SigninComponent
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import {FormBuilder} from '@angular/forms'
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  signupForm!:FormGroup
+
+  constructor(private formbuilder:FormBuilder,private http:HttpClient,private router:Router) { }
+
+  ngOnInit(): void {
+    this.signupForm=this.formbuilder.group({
+       name:[''],
+       email:[''],
+       pass:[''],
+       reppass:[''] 
+    }
+
+    );
+  }
+signup()
+{
+  this.http.post<any>('http://localhost:3000/user',this.signupForm.value).subscribe(res=>{
+    alert('gyyg')
+  })
+}
+}
